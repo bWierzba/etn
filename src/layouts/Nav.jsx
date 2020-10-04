c1d51cc29eb6f328cfbac21471f52280a171c473
wierzba <wierzbabartlomiej@gmail.com>
2020-10-04 19:15:17 +0200

    added iconify

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "../css/Nav.css";
 import logo from '../img/logo.svg'
+import { Icon } from '@iconify/react';
+import facebookIcon from '@iconify/icons-ls/facebook';
+import instagramIcon from '@iconify/icons-ls/instagram';
+
 
 const Nav = () => {
   return (
@@ -11,6 +15,9 @@ const Nav = () => {
           <li>Projekty</li>
           <li>O nas</li>
           <li>Kontakt</li>
+          <li><Icon icon={instagramIcon} /></li>
+          <li><Icon icon={facebookIcon} /></li>
+
         </ul>
       </nav>
     </div>
