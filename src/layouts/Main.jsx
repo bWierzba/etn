69338431e41c0467cd88a94cc3e2f3f35ea15243
RadHimself <radek.babij@gmail.com>
2020-10-04 18:01:23 +0200

    CSS and layouts

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Header.css";
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
---
+++ src/layouts/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Nav.css";
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
