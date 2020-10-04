e33f97e5549c9ba4d689505f3bb9bdc1d339d461
wierzba <wierzbabartlomiej@gmail.com>
2020-10-04 18:43:31 +0200

    update navbar

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "../css/Nav.css";
+import logo from '../img/logo.svg'
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className='nav-container'>
+      <nav>
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>Projekty</li>
+          <li>O nas</li>
+          <li>Kontakt</li>
+        </ul>
+      </nav>
+    </div>
+  )
 };
 
 export default Nav;
