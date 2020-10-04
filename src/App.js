1244e6e7800ac8a339dea96ee132d896a252ef98
RadHimself <radek.babij@gmail.com>
2020-10-04 17:48:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import './css/App.css'
+import React from "react";
+import "./css/App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App">Test</div>;
 }
 
 export default App;
