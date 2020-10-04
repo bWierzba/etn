1d1b1b7747376329b3ece2f5248d4b6a8440aaaa
RadHimself <radek.babij@gmail.com>
2020-10-04 18:44:05 +0200

    header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "../css/Header.css";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+import image1 from "../img/1.jpg";
+import image2 from "../img/2.jpg";
+import image3 from "../img/3.jpg";
+import image4 from "../img/3.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <AliceCarousel autoPlay autoPlayInterval="3000">
+        <img src={image1} className="sliderimg" />
+        <img src={image2} className="sliderimg" />
+        <img src={image3} className="sliderimg" />
+        <img src={image4} className="sliderimg" />
+      </AliceCarousel>
+    </div>
+  );
 };
 
 export default Header;
