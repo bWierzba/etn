c7fde43374bf187b4381271c69e70bcd6b909f65
RadHimself <radek.babij@gmail.com>
2020-10-06 16:18:42 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./css/App.css";
 import Nav from "./layouts/Nav";
 import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import Footer from "./layouts/Footer";
 
-function App() {
-  return (
-    <div>
-      <Nav />
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    console.log("They see me scrollin, they hatin");
+    window.scrollBy(0, window.innerHeight / 2);
+  };
+
+  render() {
+    return (
+      <div onScroll={this.handleScroll}>
+        <Nav />
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
