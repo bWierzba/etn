1f491601741e3f9a23df1ed7ebc80b1bc649c872
RadHimself <radek.babij@gmail.com>
2020-10-11 10:36:12 +0200

    About

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import Projects from '../pages/Projects'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
-
 import "../css/Main.css";
+
+
 const Main = () => {
   return <div className='main-container'>
     <Projects />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,49 @@
 import React from "react";
 import "../css/About.css";
-import Jagoda from "../img/Jagoda.jpg";
-import Krzysztof from "../img/Krzysztof.jpg";
-import Agnieszka from "../img/Agnieszka.jpg";
+import Jagoda from "../img/J.jpg";
+import Krzysztof from "../img/K.jpg";
+import Agnieszka from "../img/A.jpg";
 
-const About = () => {
-  return (
+let fullDescription = <div>Bylo nas 3</div>;
+let jagodaDescription = <div>Jo zech jest Jagoda</div>
+let krzysztofDescription = <div>Jo zech jest Krzysztof</div>
+let agnieszkaDescription = <div>Jo zech jest Agnieszka</div>
+
+
+class About extends React.Component {
+
+  state = {
+    description: fullDescription,
+  }
+  
+  MouseHover = (event) => {
+    console.log(event.target.id)
+    switch(event.target.id){
+    case 'Jagoda':
+      this.setState({description: jagodaDescription});
+      break;
+    case 'Krzysztof':
+      this.setState({description: krzysztofDescription});
+      break;
+    case 'Agnieszka':
+        this.setState({description: agnieszkaDescription});
+      break;
+    }
+  }
+
+  render() {
+    return (
     <div className="about-page" id="about">
       <div className="row">
         <div className="column-photos">
-          <div className="picture" ></div>
-          <div className="picture" ></div>
-          <div className="picture" ></div>
+          <div className="picture" id="Jagoda" style={{backgroundImage: `url(${Jagoda})`}} onMouseEnter={this.MouseHover} onMouseLeave={()=>this.setState({description: fullDescription})}></div>
+          <div className="picture" id="Krzysztof" style={{backgroundImage: `url(${Krzysztof})`}} onMouseEnter={this.MouseHover} onMouseLeave={()=>this.setState({description: fullDescription})}></div>
+          <div className="picture" id="Agnieszka" style={{backgroundImage: `url(${Agnieszka})`}} onMouseEnter={this.MouseHover} onMouseLeave={()=>this.setState({description: fullDescription})}></div>
         </div>
-        <div className="column-description">opis</div>
+        <div className="column-description">{this.state.description}</div>
       </div>
     </div>
   );
-};
+};}
 
 export default About;
