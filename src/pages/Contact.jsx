0e88d3e5b592a4a6cd7340b1dda1179bed6271f2
wierzba <wierzbabartlomiej@gmail.com>
2020-10-11 10:22:47 +0200

    contact page beta

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "../css/Contact.css";
 import emailjs from 'emailjs-com';
 
+
+const contacts = [
+  { city: 'warszawa', name: 'agnieszka wis', phone: '91283982173', mail: 'agnieszka@ent-studio.pl' },
+  { city: 'wrocław', name: 'jagoda hausmann', phone: '91283982173', mail: 'jagoda@ent-studio.pl' },
+  { city: 'kalisz', name: 'krzysztof urbaniak', phone: '91283982173', mail: 'krzysztof@ent-studio.pl' },
+]
+
 const Contact = () => {
 
   function sendEmail(e) {
@@ -16,17 +23,42 @@ const Contact = () => {
     e.target.reset()
   }
 
+  let contactsList = contacts.map(contact => {
+    return (
+      <div className='contact-container' key={contact.name}>
+        <h1 className='city'>{contact.city}</h1>
+        <h2>kontakt</h2>
+        <p className='name'>{contact.name}</p>
+        <p className='phone'> tel. {contact.phone}</p>
+        <p className='mail'>{contact.mail}</p>
+
+      </div>
+    )
+  })
+
 
   return <div className="contact-page" id='contact'>
-    <form className="contact-form" onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" />
-      <label>Email</label>
-      <input type="email" name="email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <h1 className='contact-page-title'>Kontakt mailowy</h1>
+    <div className='form-parent'>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <label>Twoje imie i nazwisko</label>
+        <br />
+        <input type="text" name="name" />
+        <br />
+        <label>Twój email</label>
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <label>Twoja wiadomość</label>
+        <br />
+        <textarea name="message" />
+        <br />
+        <input type="submit" value="wyślij" />
+      </form>
+    </div>
+    <div className='contacts-list'>
+      {contactsList}
+    </div>
   </div>;
 };
 
