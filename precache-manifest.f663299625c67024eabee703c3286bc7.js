d98bf685fdd566d9f03acaa9bbe00a25f140fe20
wierzba <wierzbabartlomiej@gmail.com>
2020-11-24 12:26:07 +0100

    Updates

---
+++ precache-manifest.f663299625c67024eabee703c3286bc7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d20be5d81974db5ffa180f3df6b463",
+    "url": "/etn/index.html"
+  },
+  {
+    "revision": "dabd74c0c6012ab0e59d",
+    "url": "/etn/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "0545adf57c71c179b803",
+    "url": "/etn/static/css/main.1a1115af.chunk.css"
+  },
+  {
+    "revision": "dabd74c0c6012ab0e59d",
+    "url": "/etn/static/js/2.89641382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/etn/static/js/2.89641382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0545adf57c71c179b803",
+    "url": "/etn/static/js/main.f8891c7c.chunk.js"
+  },
+  {
+    "revision": "a0b95e24191bcb740eb2",
+    "url": "/etn/static/js/runtime-main.0e6205ab.js"
+  },
+  {
+    "revision": "9668f8c9c3a51f84519b2f87ab617dcb",
+    "url": "/etn/static/media/1.9668f8c9.jpg"
+  },
+  {
+    "revision": "2ee2a5d2f7c12e1f00d63d57c9c5dd6b",
+    "url": "/etn/static/media/2.2ee2a5d2.jpg"
+  },
+  {
+    "revision": "09f452c9efaa684ba63ccb2fd8657ec2",
+    "url": "/etn/static/media/3.09f452c9.jpg"
+  },
+  {
+    "revision": "6f09b84328b514bdad8801eee8ce7533",
+    "url": "/etn/static/media/A.6f09b843.jpg"
+  },
+  {
+    "revision": "cbe66c95b99e062a1ef145b2714c8a9b",
+    "url": "/etn/static/media/J.cbe66c95.jpg"
+  },
+  {
+    "revision": "2b77f0929ce4ee65f106ff15bf04402c",
+    "url": "/etn/static/media/K.2b77f092.jpg"
+  },
+  {
+    "revision": "4adaa34cb9e71a0597582dbb5b9a3a34",
+    "url": "/etn/static/media/logo.4adaa34c.svg"
+  }
+]);
